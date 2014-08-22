9f54cc99a1eb9119c8f917f1570b6e3e05915416
Tali Herzka <taliherzka@gmail.com>
2014-08-22 14:50:24 -0700

    lying bitch

--- source/ar_truths_lies/app/controllers/controller.rb
+++ source/ar_truths_lies/app/controllers/controller.rb
@@ -1,13 +1,69 @@
-require_relative '../views/view'
+###CONTROLLER###
 
+require_relative '../views/view'
+require_relative '../../config/application'
+require_relative '../models/Student'
 
 class Controller
 
-  def self.method_call(input)
+  def initialize
+    @mc_hash = {}
+  end
+
+  def user_input(input)
+    input_string = input[0]
+    count = 0
+    View.welcome
+    current_student = Student.all[count]
+
+    until current_student.id == Student.all.length || gets.chomp == "quit"
+
+        View.display_student(current_student)
+
+        View.display_mc(displayChoices(current_student))
+        if check_answer(gets.chomp)
+          View.true
+          count += 1 
+          current_student = Student.all[count]
+        elsif !check_answer(gets.chomp) 
+          View.false
+          count += 1 
+          current_student = Student.all[count]
+        end
+        
+  
+    end
+
+    View.quit
+
+        #should break the loop and exit console
 
   end
 
-end
+
+  def displayChoices(student)
+    options = []
+
+    options << student.lie
+    student.truths.each do |truth|
+      options << truth
+    end
 
 
+    @mc_hash["A"] = options[0]
+    @mc_hash["B"] = options[1]
+    @mc_hash["C"] = options[2]
+
+  
+    return @mc_hash
+  end
+
+  def check_answer(letter)
+  
+    @mc_hash[letter].class != Truth ? true : false
+
+  end
+
+
+  end
 
--- source/ar_truths_lies/truths_and_lies.rb
+++ source/ar_truths_lies/truths_and_lies.rb
@@ -1,5 +1,10 @@
 require_relative 'config/application'
 require_relative 'app/controllers/controller'
+# require_relative '../models/Student'
+# require_relative '../models/Lie'
+# require_relative '../models/Truth'
 
-Controller.method_call(ARGV)
 
+game = Controller.new
+
+game.user_input(ARGV)
